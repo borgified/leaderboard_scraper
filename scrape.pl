bb32f206ac871a2997185f4a2b402f3c4f3646fe
Fwiffo <borgified@gmail.com>
2012-11-10 00:14:28 +0000

    needs more thought

--- scrape.pl
+++ scrape.pl
@@ -15,38 +15,42 @@ my $url		= $config{url};
 
 my $xml = XMLin(get($url));
 
-use Data::Dumper qw(Dumper);
-print Dumper $xml;
-exit;
-
-
-__END__
-
-my @players;
-
-foreach my $item (@{$$xml{'ActivePlayerData'}}){
-	push(@players,"$$item{'PlayerName'}\($$item{'Rank'}\)");
-}
-
-my $players=join(' ',@players);
 
 my $my_cnf = '/secret/my_cnf.cnf';
 
 my $dbh = DBI->connect("DBI:mysql:"
                         . ";mysql_read_default_file=$my_cnf"
-                        .';mysql_read_default_group=leaderboard_graph',
+                        .';mysql_read_default_group=scraper',
                         undef,
                         undef
                         ) or die "something went wrong ($DBI::errstr)";
 
-my $sth=$dbh->prepare("insert into currentplayers (players) values ('$players')");
-$sth->execute();
+#my $sth=$dbh->prepare("insert into currentplayers (players) values ('$players')");
+#$sth->execute();
 
-#broadcast a message out via twitter if num_players > $threshold
-my $threshold=14;
-my $num_players=@players;
+#use Data::Dumper qw(Dumper);
+#print Dumper $xml;
+#exit;
 
 
+foreach my $item (@{$$xml{'StatsLeaderboard'}}){
+
+	foreach my $cat (keys $item){
+		print "$cat $$item{$cat}\n";
+	}
+}
+
+#load lastmodified table
+
+#compare lastmodified on new leaderboard and update if different
+
+
+
+
+
+
+
+__END__
 
 my $content = get("http://acss.alleg.net/Stats/Leaderboard.aspx");
 if(!defined($content)){
